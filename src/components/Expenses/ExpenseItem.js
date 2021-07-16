f6e28fc3dd6261417d186686c3d1648010814845
Sarosh <saroshabdullah2013@gmail.com>
2021-07-17 00:32:03 +0530

    Used State on ClickListener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 // props get the data that is passed in the tag of jsx as an attribute.
 
 const ExpenseItem = (props) => {
-  const expenseTitle = props.title;
+  const [expenseTitle, setTitle] = useState(props.title);
+
+  // let expenseTitle = props.title;
   const expenseAmount = props.amount;
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+    console.log("Clicked");
+  };
 
   return (
     <Card className="expense-item">
@@ -14,8 +22,9 @@ const ExpenseItem = (props) => {
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
