5807de806fd5f3c823abd81fc8be92d40ba3067e
Sarosh <saroshabdullah2013@gmail.com>
2021-07-16 20:42:38 +0530

    Added New component and created a wrapper component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
+
+// we cant use our components as a wrapper like a div for html code by default..
+// so what we have to do is to pass the props.children basically what this does is to 
+// extract all the html code that is placed between our component tag and place it here
+// which is a basic normal nature according to html
+// so we created a wrapper class.
+// one more thing we also cannot pass multiple classes for our component tag so for that we 
+// created a classes array and then passed it here.
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 // props get the data that is passed in the tag of jsx as an attribute.
 
 function ExpenseItem(props) {
@@ -7,13 +8,13 @@ function ExpenseItem(props) {
   const expenseAmount = props.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
