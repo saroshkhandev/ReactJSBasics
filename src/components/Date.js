23aadb6736b398801a1a05de12fa133c53ff65ac
Sarosh <saroshabdullah2013@gmail.com>
2021-07-16 16:07:59 +0530

    Added a new component date and used props

---
+++ src/components/Date.js
@@ -0,0 +1,13 @@
+
+function Date(props) {
+
+    return (
+      <div>
+        <div>{props.month}</div>
+        <div>{props.year}</div>
+        <div>{props.day}</div>
+      </div>
+    );
+}
+
+export default Date;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,21 @@
 import "./ExpenseItem.css";
+import Date from "./Date";
+// props get the data that is passed in the tag of jsx as an attribute.
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 6, 15);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-Us", {
+    month: "long",
+  });
+  const day = props.date.toLocaleString("en-Us", {
+    day: "2-digit",
+  });
+  const year = props.date.getFullYear();
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <Date month={month} day={day} year={year} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
