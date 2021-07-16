4b31b996350d3fd33907c6041833f7895fa91491
Sarosh <saroshabdullah2013@gmail.com>
2021-07-16 19:47:39 +0530

    Added date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,14 @@
 import "./ExpenseItem.css";
-import Date from "./Date";
+import ExpenseDate from "./ExpenseDate";
 // props get the data that is passed in the tag of jsx as an attribute.
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-Us", {
-    month: "long",
-  });
-  const day = props.date.toLocaleString("en-Us", {
-    day: "2-digit",
-  });
-  const year = props.date.getFullYear();
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <Date month={month} day={day} year={year} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
