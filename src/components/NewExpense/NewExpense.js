73bb588db233d2f938ab3aab73a0bad474415cab
Sarosh <saroshabdullah2013@gmail.com>
2021-07-18 00:35:28 +0530

    Added states for input data

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,16 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+      const expenseData = {
+          ...enteredExpenseData,
+          id: Math.random().toString()
+      };
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
     </div>
   );
 };
